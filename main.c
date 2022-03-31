c20600c911135eca2fb6aac73a204bdd2151b336
zakaria-root <elmourtazakezia@gmail.com>
2022-03-31 17:58:19 +0000

    feat: add main

--- main.c
+++ main.c
@@ -1,32 +1,83 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "contactTableImplementation.h"
-
-
 
 int main(int argc, char const *argv[])
 {
     contact_t c1;
     contactTable_t *table;
     table->nbGrpContact = 26;
-    printf("entre le nom d'un contact : ");
-    scanf("%s",c1.nom);
-    printf("entre le tel d'un contact : ");
-    scanf("%s",c1.tel);
-    printf("entre le email d'un contact : ");
-    scanf("%s",c1.email);
+    char c;
+    do{
+    printf("1) ajouter un contact\n");
+    printf("2) supprimer un contact\n");
+    printf("3) modifier un contact\n");
+    printf("4) rechercher un contact\n");
+    printf("5) sortie de programme\n");
+    printf("enter un nimero :");
+    scanf("%c", &c1);
+    switch (c)
+    {
+    case 1:
+        system("clear");
+        printf("entre le nom d'un contact : ");
+        scanf("%s", c1.nom);
+        printf("entre le tel d'un contact : ");
+        scanf("%s", c1.tel);
+        printf("entre le email d'un contact : ");
+        scanf("%s", c1.email);
+        table = ajouterContact(table, c1);
+        afficherContact(table);
+        getchar();
+        break;
 
-    table = ajouterContact(table, c1);
-    afficherContact(table);
-    contact_t *contactChercher = chercherContact(table, c1);
-    afficherContact(contactChercher);
+    case 2:
+        system("clear");
+        printf("entre le nom d'un contact : ");
+        scanf("%s", c1.nom);
+        printf("entre le tel d'un contact : ");
+        scanf("%s", c1.tel);
+        printf("entre le email d'un contact : ");
+        scanf("%s", c1.email);
+        table = supprimerContact(table, c1);
+        afficherContact(table);
+        getchar();
+        break;
 
-    table = supprimerContact(table,c1);
-    afficherContact(table);
-    table = modifierContact(table, c1,c1);
-    afficherContact(table);
+    case 3:
+        system("clear");
+        printf("entre le nom d'un contact : ");
+        scanf("%s", c1.nom);
+        printf("entre le tel d'un contact : ");
+        scanf("%s", c1.tel);
+        printf("entre le email d'un contact : ");
+        scanf("%s", c1.email);
+        table = modifierContact(table, c1);
+        afficherContact(table);
+        getchar();
+        break;
 
-
+    case 4:
+        system("clear");
+        contactList_t *contact = NULL;
+        printf("entre le nom d'un contact : ");
+        scanf("%s", c1.nom);
+        printf("entre le tel d'un contact : ");
+        scanf("%s", c1.tel);
+        printf("entre le email d'un contact : ");
+        scanf("%s", c1.email);
+        contact = chercherContact(table, c1);
+        printf("nom :%s - tel:%s - email:%s --> ", contact->contact.nom, contact->contact.tel, contact->contact.email);
+        getchar();
+        break;
+ case 5:
+ printf("merci >>>>");
+return 0;
+ break;
+    default:
+        break;
+    }
+    }while (1);
 
 
 
